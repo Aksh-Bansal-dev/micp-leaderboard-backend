ec8828fa9f3025197850875ec4b1d4865c3a6e02
Aksh Bansal <akshmark@gmail.com>
2021-12-05 18:00:09 +0530

    refactor: add cf group url to environment variables

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import { loadCheerio } from "./loadCheerio";
 import updateDB from "./utils/updateDB";
 require("dotenv").config();
 
-const url = "https://codeforces.com/group/TImmUza0J0/members";
+const url = process.env.CF_GROUP_URL;
 
 (async () => {
-  const $ = await loadCheerio(url);
+  const $ = await loadCheerio(url!);
   const toper = await $(`.rated-user`);
   const set: Set<string> = new Set();
   toper.each((_i, e) => {
